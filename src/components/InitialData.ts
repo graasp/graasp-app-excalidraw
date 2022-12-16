1c44a77b244370c91921264051ffe08eaf651a76
Luna Godier <luna.godier@epfl.ch>
2022-12-16 13:33:47 +0100

    fix: disabled state use

--- src/components/InitialData.ts
+++ src/components/InitialData.ts
@@ -4,11 +4,11 @@ import { AppState } from '@excalidraw/excalidraw/types/types';
 
 const getInitialData = (
   elements: readonly ExcalidrawElement[],
-  state: AppState,
+  // state: AppState,
 ): ImportedDataState => {
   const data: ImportedDataState = {
     elements,
-    appState: state,
+    // appState: state,
     scrollToContent: true,
   };
   return data;
