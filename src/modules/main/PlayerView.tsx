f99b181c968b6f644732065d3cd13378b37b7102
Jérémy La Scala <jeremy.lascala@epfl.ch>
2023-11-17 18:19:51 +0100

    fix: player view

--- src/modules/main/PlayerView.tsx
+++ src/modules/main/PlayerView.tsx
@@ -1,11 +1,22 @@
+import Paper from '@mui/material/Paper';
+
 import { PLAYER_VIEW_CY } from '@/config/selectors';
 
 import Excalidraw from '../excalidraw/Excalidraw';
 
 const PlayerView = (): JSX.Element => (
-  <div data-cy={PLAYER_VIEW_CY}>
+  <Paper
+    elevation={0}
+    sx={{
+      width: '100%',
+      height: '100%',
+      display: 'flex',
+      flexDirection: 'column',
+    }}
+    data-cy={PLAYER_VIEW_CY}
+  >
     <Excalidraw />
-  </div>
+  </Paper>
 );
 
 export default PlayerView;
